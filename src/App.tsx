42c79857c4d56a77ab9cac011c731f051517abd1
iDevYT <alphangred57@gmail.com>
2022-08-07 18:40:33 +0200

    added a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
 function App() {
   return (
-    <h1 className='text-3xl underline font-bold font-fam'>
-      Hey
-    </h1>
+    <div className='bg-sky-200 w-full h-full'>
+  <div className='bg-blue-400 text-white py-4 pl-4 m-1 rounded-md sticky top-0'>
+    <h1 className='font-bold font-mono text-xl'>Nova Courses</h1>
+  </div>
+  <div className='text-center p-12'>
+
+  <h1 className=' text-3xl font-thin font-mono'>Learn. <span className='font-extrabold text-blue-400'>Coding.</span> Now.</h1>
+  <p>Learn Coding in 5-Minute Courses</p>
+  <button className='bg-blue-700 p-2 px-7 text-white rounded-full hover:bg-sky-400 hover:text-black'>Start Now</button>
+  </div>
+    </div>
   );
 }
 
