80edeb409fb93044d2c949e8fbd76fbe200103d1
iDevYT <alphangred57@gmail.com>
2022-08-08 10:42:13 +0200

    changed some components

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,20 @@ function App() {
   </div>
   <div className='text-center p-12'>
 
-  <h1 className=' text-3xl font-thin font-mono'>Learn.<span className='font-extrabold text-blue-400'>Coding.</span>Now<span className='text-green-500'>();</span></h1>
+  <h1 className=' text-3xl font-thin font-mono'>Learn.<span className='font-extrabold text-blue-400'>Coding.</span>Now<span className='text-green-600'>();</span></h1>
   <p>Learn Coding in 5-Minute Courses</p>
   <button className='bg-blue-700 p-2 px-7 text-white rounded-full hover:bg-sky-400 hover:text-black'>Start Now</button>
   </div>
-  <div className="grid grid-rows-2 grid-cols-2 px-96">
+  <div className="grid grid-rows-2 grid-cols-2 px-9">
   <Card title='Simple To Use' text='Nova Courses is a very simple way to learn coding'/>
   <Card title='Fullstack Guaranteed' text='whith Nova Courses you can learn to make a Full-stack App!'/>
   <Card title='100% Free*' text='Nova Social Is 100% Free for now!'/>
   <Card title='Try Now' text='Try now and learn coding!'/>
   </div>
+  <footer>
+    <h1>© 2022 Nova Systems Inc.</h1>
+    <h1 className='text-right'>ToS</h1>
+  </footer>
   </div>
   );
 }
--- src/components/PolyUI.tsx
+++ src/components/PolyUI.tsx
@@ -1,11 +1,11 @@
-interface Props {
+interface CardProps {
     title: string;
     text: string;
 }
-export function Card({title, text}: Props,)
+export function Card({title, text}: CardProps,)
 {
     return (
-        <div className='bg-blue-400 text-white py-4 pl-4 m-1 rounded-md sticky top-0 w-{15%}'>
+        <div className='bg-blue-400 text-white py-4 pl-4 m-1 rounded-md sticky top-0 w-{15%} h-56 transition-all hover:bg-blue-200 hover:text-black hover:translate-y-3'>
             <h1 className='font-bold font-mono text-xl'>{title}</h1>
             <p className="font-light font-mono text-md">{text}</p>
         </div>
